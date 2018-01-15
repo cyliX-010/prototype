23556292b19b5321c0895ed8d1b2af006952f569
jerzabongco <34363144+jerzabongco@users.noreply.github.com>
2018-01-15 14:16:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,16 @@
 	</head>
 	<body>
 
-		<div id = "base" style = "position:absolute; top:0px; left:0px; height: 1000%; width:100%; background-color:gray;">
+		<div id = "base" style = "position:absolute; top:0px; left:0px; height: 1050%; width:100%; background-color:gray;">
 	<div class= "first" style = "position: relative; top: 50px; left: 50px; height: 82%; width:82%; margin: 3%;">
+	<img class = "img1"src = "0.png" style = "position: relative; top:0px; left:0px; height: 10%; width: 100%;">
 	<img class = "img1"src = "1.png" style = "position: relative; top:0px; left:0px; height: 10%; width: 100%;">
-	<img class = "img1"src = "2.png" style = "position: relative; top: 0px; left:0px; height: 10%; width: 100%;">
+	<img class = "img1"src = "2.png" style = "position: relative; top:0px; left:0px; height: 10%; width: 100%;">
 	<img class = "img1"src = "3.png" style = "position: relative; top: 0px; left:0px; height: 10%; width: 100%;">
 	<img class = "img1"src = "4.png" style = "position: relative; top: 0px; left:0px; height: 10%; width: 100%;">
 	<img class = "img1"src = "5.png" style = "position: relative; top: 0px; left:0px; height: 10%; width: 100%;">
+	<img class = "img1"src = "6.png" style = "position: relative; top: 0px; left:0px; height: 10%; width: 100%;">
+	<img class = "img1"src = "7.png" style = "position: relative; top:0px; left:0px; height: 10%; width: 100%;">
 	
 	
 	</div>
